11ba2481f6768ff4f1f44aed120b2daf484d4f91
thevtn <73998182+thevtn@users.noreply.github.com>
2023-04-17 09:09:47 +0300

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,8 +1,11 @@
 #!/system/bin/sh
 {
+	MODDIR=${0%/*}
+	MODULES=${MODDIR%/*}
+	NVBASE=${MODULES%/*}
 	rm $NVBASE/rvhc/__PKGNAME_rv.apk
 	rmdir $NVBASE/rvhc
-	if __ISBNDL :; then
+	if  :; then
 		until [ "$(getprop sys.boot_completed)" = 1 ]; do sleep 1; done
 		sleep 15
 		pm uninstall __PKGNAME
