1e5736f2f32a5881f817c69b11493216c0eb6b18
thevtn <73998182+thevtn@users.noreply.github.com>
2023-04-17 09:07:59 +0300

    Update customize.sh

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,24 +1,13 @@
 # shellcheck disable=SC2148,SC2086
 ui_print ""
 
-if [ $ARCH = "arm" ]; then
-	#arm
-	ARCH_LIB=armeabi-v7a
-	alias cmpr='$MODPATH/bin/arm/cmpr'
-elif [ $ARCH = "arm64" ]; then
-	#arm64
-	ARCH_LIB=arm64-v8a
-	alias cmpr='$MODPATH/bin/arm64/cmpr'
-else
-	abort "ERROR: unsupported arch: ${ARCH}"
-fi
 set_perm_recursive $MODPATH/bin 0 0 0755 0777
 
-nsenter -t1 -m -- grep __PKGNAME /proc/mounts | while read -r line; do
+nsenter -t1 -m grep __PKGNAME /proc/mounts | while read -r line; do
 	ui_print "* Un-mount"
 	mp=${line#* }
 	mp=${mp%% *}
-	nsenter -t1 -m -- umount -l "${mp%%\\*}"
+	nsenter -t1 -m umount -l ${mp%%\\*}
 done
 am force-stop __PKGNAME
 
@@ -38,10 +27,9 @@ if BASEPATH=$(pm path __PKGNAME); then
 fi
 if [ $INS = true ]; then
 	ui_print "* Updating __PKGNAME (v__PKGVER)"
-	settings put global verifier_verify_adb_installs 0
 	SZ=$(stat -c "%s" $MODPATH/__PKGNAME.apk)
 	if ! SES=$(pm install-create --user 0 -i com.android.vending -r -d -S "$SZ" 2>&1); then
-		ui_print "ERROR: install-create failed"
+		ui_print "ERROR: session creation failed"
 		abort "$SES"
 	fi
 	SES=${SES#*[}
@@ -55,7 +43,6 @@ if [ $INS = true ]; then
 		ui_print "ERROR: install-commit failed"
 		abort "$op"
 	fi
-	settings put global verifier_verify_adb_installs 1
 	if BASEPATH=$(pm path __PKGNAME); then
 		BASEPATH=${BASEPATH##*:}
 		BASEPATH=${BASEPATH%/*}
@@ -67,21 +54,21 @@ BASEPATHLIB=${BASEPATH}/lib/${ARCH}
 if [ -z "$(ls -A1 ${BASEPATHLIB})" ]; then
 	ui_print "* Extracting native libs"
 	mkdir -p $BASEPATHLIB
-	if ! op=$(unzip -j $MODPATH/__EXTRCT lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
+	if ! op=$(unzip -j $MODPATH/__EXTRCT.apk lib/${ARCH_LIB}/* -d ${BASEPATHLIB} 2>&1); then
 		ui_print "ERROR: extracting native libs failed"
 		abort "$op"
 	fi
-	set_perm_recursive ${BASEPATH}/lib 1000 1000 755 755 u:object_r:apk_data_file:s0
+	set_perm_recursive ${BASEPATHLIB} 1000 1000 755 755 u:object_r:apk_data_file:s0
 fi
 ui_print "* Setting Permissions"
 set_perm $MODPATH/base.apk 1000 1000 644 u:object_r:apk_data_file:s0
 
 ui_print "* Mounting __PKGNAME"
-mkdir -p $NVBASE/rvhc
+mkdir $NVBASE/rvhc 2>/dev/null
 RVPATH=$NVBASE/rvhc/__PKGNAME_rv.apk
 mv -f $MODPATH/base.apk $RVPATH
 
-if ! op=$(nsenter -t1 -m -- mount -o bind $RVPATH $BASEPATH/base.apk 2>&1); then
+if ! op=$(nsenter -t1 -m mount -o bind $RVPATH $BASEPATH/base.apk 2>&1); then
 	ui_print "ERROR: Mount failed!"
 	ui_print "$op"
 fi
@@ -92,10 +79,6 @@ nohup cmd package compile --reset __PKGNAME >/dev/null 2>&1 &
 ui_print "* Cleanup"
 rm -rf $MODPATH/bin $MODPATH/__PKGNAME.apk
 
-for s in "uninstall.sh" "service.sh"; do
-	sed -i "2 i\NVBASE=${NVBASE}" $MODPATH/$s
-done
-
 ui_print "* Done"
-ui_print "  by j-hc (github.com/j-hc)"
+ui_print "  by j-hc & thevtn"
 ui_print " "
